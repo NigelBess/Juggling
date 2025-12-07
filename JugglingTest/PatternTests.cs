47419d79a85a4812f442b4bed5ba7036fe68b1ac
MainUser <nigelbess@gmail.com>
2025-12-06 21:04:37 -0800

    allow hands to hold multiple balls

--- JugglingTest/PatternTests.cs
+++ JugglingTest/PatternTests.cs
@@ -18,21 +18,12 @@ public sealed class PatternTests
     public void InvalidPattern2_IsInvalid() => TestSingleHandPatternValidity(InvalidPattern2(), false);
 
     [TestMethod]
-    public void InvalidPattern3_IsInvalid() => TestSingleHandPatternValidity(InvalidPattern3(), false);
+    public void ThrowTwoBallsFromTheSameHand_IsValid() => TestSingleHandPatternValidity(ThrowTwoBallsFromTheSameHand(), true);
 
     public void TestSingleHandPatternValidity(HandPattern handPattern, bool expectedValid)
     {
         var pattern = new Pattern() { Hands = [handPattern] };
         Assert.AreEqual(expectedValid, pattern.TryGetStateSequence(out _, out var errorMessage), errorMessage);
-    }
-
-    public static IEnumerable<object[]> GetPatterns()
-    {
-        yield return new object[] { SingleBallToss(), true };
-        yield return new object[] { ValidPattern2(), true };
-        yield return new object[] { InvalidPattern1(), false };
-        yield return new object[] { InvalidPattern2(), false };
-        yield return new object[] { InvalidPattern3(), false };
     }
 
     private static HandPattern InvalidPattern1() => new()
@@ -52,7 +43,7 @@ public sealed class PatternTests
             new() { X = 0, Y = 0, ActionType = HandActionType.Catch, Ball = 1 }
         ]
     };
-    private static HandPattern InvalidPattern3() => new()
+    private static HandPattern ThrowTwoBallsFromTheSameHand() => new()
     {
         // Throwing a different ball from the one you just caught
         Actions =
